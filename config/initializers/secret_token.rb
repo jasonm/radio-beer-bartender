917ae4d00a5b3b0bdc32755cd2f7b83bb2c8ae0f
Jason Morrison <jason.p.morrison@gmail.com>
2013-02-09 20:52:49 -0800

    initial rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bartender::Application.config.secret_token = 'b72f208ac19c36d75dd5879729cf35834087997cfb2b634fa728420b303b7775f522c9f146cb367b20291b2b9c344d16211f26afd23a387a20219d1d9252205e'
