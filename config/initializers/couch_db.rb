fd35019206c43d46bf32d98f723685d94a81c876
Jason Morrison <jason.p.morrison@gmail.com>
2013-02-09 22:32:52 -0800

    more configging

--- config/initializers/couch_db.rb
+++ config/initializers/couch_db.rb
@@ -5,9 +5,11 @@ JSON.parser = JSON::Ext::Parser
 require 'couchrest'
 
 config = YAML.load(Rails.root.join('config/couchdb.yml').read)
-couch_db_url = ENV['CLOUDANT_URL'] || ENV['COUCH_DB_URL'] || config[Rails.env]
-if couch_db_url
-  $db = CouchRest.database!(couch_db_url)
+url_root = ENV['CLOUDANT_URL'] || ENV['COUCH_DB_URL'] || config[Rails.env]['url']
+database = ENV['COUCH_DATABASE_NAME'] || config[Rails.env]['database']
+
+if url_root && database
+  $db = CouchRest.database!(url_root + '/' + database)
 else
   Rails.logger.info("Skipping CouchDB initialization since no config present")
 end
