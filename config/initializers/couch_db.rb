86cf9c074ed62d65c459090322164cf3436868ca
Jason Morrison <jason.p.morrison@gmail.com>
2013-02-09 22:21:23 -0800

    Make couchdb config optional, esp for heroku asset compilation

--- config/initializers/couch_db.rb
+++ config/initializers/couch_db.rb
@@ -6,4 +6,8 @@ require 'couchrest'
 
 config = YAML.load(Rails.root.join('config/couchdb.yml').read, safe: true)
 couch_db_url = ENV['CLOUDANT_URL'] || ENV['COUCH_DB_URL'] || config[Rails.env]
-$db = CouchRest.database!(couch_db_url)
+if couch_db_url
+  $db = CouchRest.database!(couch_db_url)
+else
+  Rails.logger.info("Skipping CouchDB initialization since no config present")
+end
