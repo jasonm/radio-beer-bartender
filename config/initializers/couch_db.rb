90bd834e10a6181e58e15cb933df8c2303c18a1b
Jason Morrison <jason.p.morrison@gmail.com>
2013-02-09 22:22:04 -0800

    fix yaml parse syntax, no safe:true

--- config/initializers/couch_db.rb
+++ config/initializers/couch_db.rb
@@ -4,7 +4,7 @@ JSON.parser = JSON::Ext::Parser
 
 require 'couchrest'
 
-config = YAML.load(Rails.root.join('config/couchdb.yml').read, safe: true)
+config = YAML.load(Rails.root.join('config/couchdb.yml').read)
 couch_db_url = ENV['CLOUDANT_URL'] || ENV['COUCH_DB_URL'] || config[Rails.env]
 if couch_db_url
   $db = CouchRest.database!(couch_db_url)
