454be6a71f66ff681302fca7d4f7771ef383ff4c
Jason Morrison <jason.p.morrison@gmail.com>
2013-02-09 22:05:31 -0800

    Basic structure stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def body_class
+    qualified_controller_name = controller.controller_path.gsub('/','-')
+    "#{qualified_controller_name} #{qualified_controller_name}-#{controller.action_name}"
+  end
+
+  def nav_link_to(content, path_or_options)
+    li_options =
+      if current_page?(path_or_options)
+        { class: 'active' }
+      else
+        { }
+      end
+
+    content_tag('li', li_options) do
+      link_to(content, path_or_options)
+    end
+  end
 end
