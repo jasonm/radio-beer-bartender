b5b389f08c8f3ce638a1bf76fccfde9e3bc1dac1
Jason Morrison <jason.p.morrison@gmail.com>
2013-02-09 22:15:45 -0800

    prod-only http user auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :authenticate
+
+  protected
+
+  def authenticate
+    if Rails.env.production? && ENV['HTTP_AUTH_USER'] && ENV['HTTP_AUTH_PASSWORD']
+      authenticate_or_request_with_http_basic do |username, password|
+        username == ENV['HTTP_AUTH_USER'] && password == ENV['HTTP_AUTH_PASSWORD']
+      end
+    else
+      true
+    end
+  end
 end
